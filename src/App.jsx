9ee2c93d35a5162e410a8e68fe5f2d7210392fac
Alejandro Cayoja <Stronranger@gmail.com>
2021-05-07 15:52:11 -0300

    Creacion del sector visual con fondo de pantalla y el sector de preguntas

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,27 @@ import './App.css';
 function App() {
   return (
     <div className="app">
-      <div className="main"></div>
-      <div className="pyramid"></div>
+      <div className="main">Menu</div>
+      <div className="pyramid">
+        <ul className="List">
+          <li className="ListItem active">
+            <span className="ListItemNumber">4</span>
+            <span className="ListItemAmount">Seguis en racha wacho</span>
+          </li>
+          <li className="ListItem">
+            <span className="ListItemNumber">3</span>
+            <span className="ListItemAmount">En racha prro</span>
+          </li>
+          <li className="ListItem">
+            <span className="ListItemNumber">2</span>
+            <span className="ListItemAmount">Bien</span>
+          </li>
+          <li className="ListItem">
+            <span className="ListItemNumber">1</span>
+            <span className="ListItemAmount">Nada mal</span>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
