130940e9df60f30cab034398fec40d5084aca0cd
Alejandro Cayoja <Stronranger@gmail.com>
2021-05-07 16:58:07 -0300

    Mejoras en lo visual y creacion de un contador sin funcion

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,36 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [questionNumber, setQuestionNumber] = useState (1)
+  const ListPyramid = [
+    {id:1, amount:"Nada mal"},
+    {id:2, amount:"Bien"},
+    {id:3, amount:"En racha prro"},
+    {id:4, amount:"Seguis en racha wacho"},
+    {id:5, amount:"5/10 mira vos"},
+    {id:6, amount:"Ahora se complica prro"},
+    {id:7, amount:"Vamo a jugar"},
+    {id:8, amount:"Ya tu sabes"},
+    {id:9, amount:"No la pechees"},
+    {id:10, amount:"MVP"},
+  ].reverse();
   return (
     <div className="app">
-      <div className="main">Menu</div>
+      <div className="main">
+        <div className="top">
+          <div className="timmer">30</div>
+        </div>
+        <div className="bottom">question and answer</div>
+      </div>
       <div className="pyramid">
         <ul className="List">
-          <li className="ListItem active">
-            <span className="ListItemNumber">4</span>
-            <span className="ListItemAmount">Seguis en racha wacho</span>
-          </li>
-          <li className="ListItem">
-            <span className="ListItemNumber">3</span>
-            <span className="ListItemAmount">En racha prro</span>
-          </li>
-          <li className="ListItem">
-            <span className="ListItemNumber">2</span>
-            <span className="ListItemAmount">Bien</span>
-          </li>
-          <li className="ListItem">
-            <span className="ListItemNumber">1</span>
-            <span className="ListItemAmount">Nada mal</span>
-          </li>
+          {ListPyramid.map((m) => (
+            <li className={questionNumber === m.id ? "ListItem active" : "ListItem"} >
+              <span className="ListItemNumber">{m.id}</span>
+              <span className="ListItemAmount">{m.amount}</span>
+            </li>
+          ))}
         </ul>
       </div>
     </div>
