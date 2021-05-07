d04dc03b5951efffe46c6c13af9c2fbed2479662
Alejandro Cayoja <Stronranger@gmail.com>
2021-05-07 14:42:22 -0300

    Modificacion de la base de los archivos

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <div className="main"></div>
+      <div className="pyramid"></div>
+    </div>
+  );
+}
+
+export default App;
