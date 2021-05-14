baa924d87055da3466674205a9ed9d3d172d575b
Alejandro Cayoja <Stronranger@gmail.com>
2021-05-14 11:08:23 -0300

    Agregando logica del juego

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import Trivia from './components/Trivia';
 
 function App() {
   const [questionNumber, setQuestionNumber] = useState (1);
   const [timeOut, setTimeOut] = useState (false);
+  const [earned, setEarned] = useState ("Nada mal");
 
   const data = [
     {
@@ -76,30 +77,42 @@ function App() {
   ]
 
   const ListPyramid = [
-    {id:1, amount:"Nada mal"},
-    {id:2, amount:"Bien"},
-    {id:3, amount:"En racha prro"},
-    {id:4, amount:"Seguis en racha wacho"},
-    {id:5, amount:"5/10 mira vos"},
-    {id:6, amount:"Ahora se complica prro"},
-    {id:7, amount:"Vamo a jugar"},
-    {id:8, amount:"Ya tu sabes"},
-    {id:9, amount:"No la pechees"},
-    {id:10, amount:"MVP"},
+    {id:1, amount:"10 - Nada mal"},
+    {id:2, amount:"9 - Bien"},
+    {id:3, amount:"8 - En racha prro"},
+    {id:4, amount:"7 - Seguis en racha wacho"},
+    {id:5, amount:"6 - 5/10 mira vos"},
+    {id:6, amount:"5 - Ahora se complica prro"},
+    {id:7, amount:"4 - Vamo a jugar"},
+    {id:8, amount:"3 - Ya tu sabes"},
+    {id:9, amount:"2 - No la pechees"},
+    {id:10, amount:"1 - MVP"},
   ].reverse();
+
+  useEffect(() => {
+    questionNumber > 1 && 
+      setEarned(ListPyramid.find((m) => m.id === questionNumber - 1).amount);
+  }, [ListPyramid, questionNumber]);
   return (
     <div className="app">
       <div className="main">
-        <div className="top">
-          <div className="timmer">30</div>
-        </div>
-        <div className="bottom">
-          <Trivia data={data} 
-          setTimeOut={setTimeOut} 
-          questionNumber={questionNumber}
-          setQuestionNumber={setQuestionNumber} 
-          />
-        </div>
+        {timeOut ? (
+          <h1 className="endText">Fallastes,<br/>llegastes hasta la <br/>posicion: {earned} </h1>
+        ) : (
+          <>
+            <div className="top">
+              <div className="timmer">30</div>
+            </div>
+            <div className="bottom">
+              <Trivia 
+                data={data} 
+                setTimeOut={setTimeOut} 
+                questionNumber={questionNumber}
+                setQuestionNumber={setQuestionNumber} 
+              />
+            </div>
+          </>
+        )}
       </div>
       <div className="pyramid">
         <ul className="List">
